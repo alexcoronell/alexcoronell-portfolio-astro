ce2a090f582f44d47b36fc5235c9a23195c6342c
Alex Coronell <alexcoronell@gmail.com>
2024-01-22 18:55:26 -0500

    Basic palette color applied on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,astro}",
   ],
   theme: {
+    colors: {
+      'transparent': 'transparent',
+      'primary': '#42b883',
+      'primary-dark': '#21262b',
+      'black': '#000000',
+      'white': '#FFFFFF',
+      'red': 'FF0000',
+    },
+    screens: {
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1200px',
+      '2xl': '1400px',
+      '3xl': '1600px',
+    },
     extend: {},
   },
   plugins: [],
