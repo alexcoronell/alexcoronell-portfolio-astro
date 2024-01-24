1f082aab2a60354aa55224caab0a3ed5595a5ed1
Alex Coronell <alexcoronell@gmail.com>
2024-01-23 19:32:23 -0500

    Presentation partial completed

--- src/core/data/personalData.ts
+++ src/core/data/personalData.ts
@@ -1,6 +1,7 @@
 const PersonalData = {
     name: 'Alex Coronell',
-    fullName: 'Alexander Coronell'
+    fullName: 'Alexander Coronell',
+    shortPresentation: "I'm a full-stack developer with 4 years of experience. I'm primarily focused on frontend development, but I also have experience with backend development."
 }
 
 export default PersonalData
