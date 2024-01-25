041ec35c2c3b2a70080db878ea75ac1d8512de5c
Alex Coronell <alexcoronell@gmail.com>
2024-01-25 16:58:16 -0500

    AboutMe section partial completed

--- src/core/data/personalData.ts
+++ src/core/data/personalData.ts
@@ -1,7 +1,13 @@
 const PersonalData = {
-    name: 'Alex Coronell',
+    shortname: 'Alex Coronell',
     fullName: 'Alexander Coronell',
-    shortPresentation: "I'm a full-stack developer with 4 years of experience. I'm primarily focused on frontend development, but I also have experience with backend development."
+    shortPresentation: "I'm a full-stack developer with 4 years of experience. I'm primarily focused on frontend development, but I also have experience with backend development.",
+    titleLongPresentation: 'Pixels are my passion, servers my playground: Meet Alex Coronell!',
+    longPresentation: [
+        "I'm Alex Coronell, a full-stack developer with a front-end soul. Like a digital architect, I sculpt pixel-perfect interfaces using Angular, React, and the rising star, Astro, pushing the boundaries of web experiences with every line of code. But don't be fooled by the flashy front-end façade – I'm also a backend apprentice, deciphering the magic of NestJS and NodeJS to build robust server-side foundations.",
+        "Sure, I may still be mastering some server-side spells, but my passion for crafting seamless, full-stack solutions burns bright. For me, code is a symphony of logic and creativity, where solving challenges is an art form. Whether it's building captivating UIs, untangling backend logic, or venturing into the realm of desktop apps with Electron, every project is a canvas waiting to be splashed with innovation.",
+        "But I'm not just a solo developer; I'm a collaborator, a constant learner fueled by the power of \"we.\" My journey in tech is just beginning, but my boundless curiosity and relentless drive keep me pushing the boundaries. So, whether you need a front-end magician to paint your digital vision, a backend apprentice eager to learn, or simply a passionate teammate to code alongside, I'm your guy. Let's build something incredible together!"
+    ]
 }
 
 export default PersonalData
