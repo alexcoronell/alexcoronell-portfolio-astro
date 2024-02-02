e15dc2bc68c534cd0688024881791a3ac81940a1
Alex Coronell <alexcoronell@gmail.com>
2024-02-01 19:39:57 -0500

    Experiences incomplete functionality with bugs

--- src/components/Home/Experiences.tsx
+++ src/components/Home/Experiences.tsx
@@ -4,19 +4,48 @@ import { type Experience } from "../../core/types/PersonalData";
 import "./_Experiences.scss";
 
 const Experiences = () => {
-    /* const { currentExperience, setCurrentExperience } = useState<Experience | undefined>(undefined) */
   const { experiences } = personalData;
+  const [currentExperienceIndex, setCurrentExperienceIndex] =useState<number>(0);
+  const [currentExperience, setCurrentExperience] = useState<Experience>(experiences[currentExperienceIndex]);
+
+  const handleClick = (index: number): void => {
+    setCurrentExperienceIndex(index)
+    setCurrentExperience(experiences[currentExperienceIndex])
+    console.log('Button clicked!', index);
+  }
   return (
     <div className="Experiences">
       <div className="Experiences__business">
         {experiences.map((item, index) => (
           <button
             class="Experiences__business__button"
-            id={item.business + "-btn-" + index}
+            id={"-btn-" + index}
+            onClick={() => handleClick(index)}
           >
             {item.business}
           </button>
         ))}
+      </div>
+      <div className="Experiences__details">
+        <div className="Experiences__details__header">
+            <h4>{ currentExperience?.title }</h4>
+            -
+            <h5>{ currentExperience?.business }</h5>
+        </div>
+        <div className="Experiences__details__date">
+            <p>{ currentExperience?.dateBegin } - { currentExperience?.dateEnd }</p>
+        </div>
+        <div className="Experiences__details__functions">
+            <ul>
+                {
+                    currentExperience?.functions?.map(item => {
+                        return (
+                            <li>punto - <p>{item}</p></li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
       </div>
     </div>
   );
