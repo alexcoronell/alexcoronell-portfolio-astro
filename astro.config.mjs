c173ed2a31b3017bf2dac7703d51c6efb764fef4
Alex Coronell <alexander@catapultsteel.com>
2024-06-18 12:54:17 -0500

    Basic implementation es language - translate data not yet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,31 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 import preact from "@astrojs/preact";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://alexcoronell.dev/',
-  integrations: [tailwind({
-    nesting: true,
-    applyBaseStyles: false
-  }), preact(), sitemap()],
-  output: 'server',
+  site: "https://alexcoronell.dev/",
+  integrations: [
+    tailwind({
+      nesting: true,
+      applyBaseStyles: false,
+    }),
+    preact(),
+    sitemap(),
+  ],
+  i18n: {
+    defaultLocale: "en",
+    locales: ['en', 'es'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
+  output: "server",
   adapter: cloudflare(),
 });
